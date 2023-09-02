93624b04b032e91ed7192343403b582b739c6471
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-02 06:57:19 +0530

    revise

--- Array/index.js
+++ Array/index.js
@@ -1,25 +1,26 @@
-function printTwoElements(arr) {
-    const n = arr.length;
-    const temp = Array(n).fill(0); // Creating temp array of size n with initial values as 0.
-    let repeatingNumber = -1;
-    let missingNumber = -1;
+// function printTwoElements(arr) {
+//     const n = arr.length;
+//     const temp = Array(n).fill(0); // Creating temp array of size n with initial values as 0.
+//     let repeatingNumber = -1;
+//     let missingNumber = -1;
 
-    for (let i = 0; i < n; i++) {
-        temp[arr[i] - 1]++;
-        if (temp[arr[i] - 1] > 1) {
-            repeatingNumber = arr[i];
-        }
-    }
-    for (let i = 0; i < n; i++) {
-        if (temp[i] === 0) {
-            missingNumber = i + 1;
-            break;
-        }
-    }
+//     for (let i = 0; i < n; i++) {
+//         temp[arr[i] - 1]++;
+//         if (temp[arr[i] - 1] > 1) {
+//             repeatingNumber = arr[i];
+//         }
+//     }
+//     for (let i = 0; i < n; i++) {
+//         if (temp[i] === 0) {
+//             missingNumber = i + 1;
+//             break;
+//         }
+//     }
 
-    console.log(`The repeating number is ${repeatingNumber}.`);
-    console.log(`The missing number is ${missingNumber}.`);
-}
+//     console.log(`The repeating number is ${repeatingNumber}.`);
+//     console.log(`The missing number is ${missingNumber}.`);
+// }
 
-const arr = [8, 3, 4, 5, 5, 6, 2];
-printTwoElements(arr);
+// const arr = [8, 3, 4, 5, 5, 6, 2];
+// printTwoElements(arr);
+
