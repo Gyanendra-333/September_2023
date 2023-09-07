7d33266ca1535c23456ff7191e064574f38cd301
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-07 06:05:21 +0530

    revsion

---
+++ Matrix/index.js
@@ -0,0 +1,22 @@
+
+// let arr = [[1, 2, 3, 4],
+// [5, 6, 7, 8],
+// [9, 10, 11, 12]];
+
+// for (let i = 0; i < 3; i++) {
+//     let s = "";
+
+//     for (let j = 0; j < 4; j++) {
+//         s += (arr[i][j] + " ");
+//     }
+//     console.log(s);
+// }
+
+
+
+
+
+function searchInMatrix(arr, x) {
+    let m = arr.length, n = arr[0].length;
+
+}
