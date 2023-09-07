2e86b62b61b785defb7201655714bc3e9bb5078d
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-07 06:16:32 +0530

    revsion

--- Matrix/index.js
+++ Matrix/index.js
@@ -19,4 +19,21 @@
 function searchInMatrix(arr, x) {
     let m = arr.length, n = arr[0].length;
 
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (arr[i][j] == x) return true;
+        }
+    }
+    return false;
+}
+let x = 5;
+let arr = [[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12]];
+
+if (searchInMatrix(arr, x)) {
+    console.log("Present Element");
+}
+else {
+    console.log("Not Presents Elements");
 }
