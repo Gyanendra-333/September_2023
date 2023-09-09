aca58cb650bcee58efc5833d0fa0aed6595f3da5
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-09 07:22:22 +0530

    revsion

--- Matrix/index.js
+++ Matrix/index.js
@@ -121,3 +121,38 @@
 
 
 
+let R = 4;
+let C = 4;
+
+function reverseColumns(arr) {
+    for (let i = 0; i < C; i++)
+        for (let j = 0, k = C - 1; j < k; j++, k--) {
+            let x = arr[j][i];
+            arr[j][i] = arr[k][i];
+            arr[k][i] = x;
+        }
+
+}
+function transpose(arr) {
+    for (let i = 0; i < R; i++)
+        for (let j = i; j < C; j++) {
+            let x = arr[j][i];
+            arr[j][i] = arr[i][j];
+            arr[i][j] = x;
+        }
+}
+function printMatrix(arr) {
+    console.log(arr);
+}
+function rotate180(arr) {
+    transpose(arr);
+    reverseColumns(arr);
+    transpose(arr);
+    reverseColumns(arr);
+}
+let arr = [[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+[13, 14, 15, 16]];
+rotate180(arr);
+printMatrix(arr);
