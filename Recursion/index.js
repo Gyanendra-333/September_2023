ccad3b3a57cab5b8b376be4763c031be63289ece
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-13 01:25:32 +0530

    practice

---
+++ Recursion/index.js
@@ -0,0 +1,28 @@
+// function printFun(test) {
+//     if (test < 1) return;
+
+//     else {
+//         console.log(test + " ");
+//         printFun(test - 1);
+//         console.log(test + " ");
+//         return;
+//     }
+// }
+// let test = 4;
+// printFun(test);
+
+
+
+function fib(n) {
+
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+let n = 5;
+console.log("Fibonacci Series of Numbers is : ");
+
+for (let i = 0; i < n; i++) {
+    console.log(fib(i));
+}
