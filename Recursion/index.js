64492b41bf575ec4e4ebaee8b398b0c5b9991253
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-15 06:02:38 +0530

    practice

--- Recursion/index.js
+++ Recursion/index.js
@@ -31,12 +31,59 @@
 
 
 
-function fact(n) {
+// function fact(n) {
 
-    if (n == 0 || n == 1) return 1;
+//     if (n == 0 || n == 1) return 1;
 
-    else
-        return n * fact(n - 1);
+//     else
+//         return n * fact(n - 1);
+// }
+// let n = 5;
+// console.log("Factorial of " + n + " is : " + fact(n));
+
+
+
+
+
+// function towerOfHanoi(n, from_rod, to_rod, aux_rod) {
+
+//     if (n == 0) return;
+
+//     towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+//     console.log("Move Disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+//     towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+// }
+// let N = 3;
+
+// towerOfHanoi(N, "A", "B", "C");
+
+
+
+
+
+
+
+// function printNos(n) {
+//     if (n > 0) {
+//         printNos(n - 1);
+//         console.log(n);
+//     }
+//     return;
+// }
+// let n = 20;
+// printNos(n);
+
+
+
+
+
+function printNos(n) {
+    if (n > 0) {
+        console.log(n);
+        printNos(n - 1);
+    }
+    return;
 }
-let n = 5;
-console.log("Factorial of " + n + " is : " + fact(n));
+
+var n = 10;
+printNos(n);
