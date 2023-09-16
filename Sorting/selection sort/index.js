e973f34e4d3729a82e34d2a8e9779b8481f567cb
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-16 07:02:41 +0530

    Bubble Sort

--- Sorting/selection sort/index.js
+++ Sorting/selection sort/index.js
@@ -38,3 +38,33 @@
 
 
 
+function bubbleSort(arr, n) {
+
+    let temp, swapped;
+
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+function printArray(arr, size) {
+    for (let i = 0; i < size; i++)
+        console.log(arr[i]);
+}
+let arr = [64, 34, 25, 12, 22, 11, 90];
+let n = arr.length;
+
+bubbleSort(arr, n);
+console.log("Sorted Array : ");
+printArray(arr, n);
