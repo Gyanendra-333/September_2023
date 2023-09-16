38401b38b3715764a60a2c8b721c234e742f064e
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-16 06:15:06 +0530

    revsion

--- Sorting/selection sort/index.js
+++ Sorting/selection sort/index.js
@@ -1,33 +1,40 @@
-function swap(arr, xp, yp) {
-    let temp = arr[xp];
-    arr[xp] = arr[yp];
-    arr[yp] = temp;
-}
-function selectionSort(arr, n) {
+// function swap(arr, xp, yp) {
+//     let temp = arr[xp];
+//     arr[xp] = arr[yp];
+//     arr[yp] = temp;
+// }
+// function selectionSort(arr, n) {
 
-    let i, j, min_idx;
+//     let i, j, min_idx;
 
-    for (let i = 0; i < n - 1; i++) {
-        min_idx = i;
+//     for (let i = 0; i < n - 1; i++) {
+//         min_idx = i;
 
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[min_idx])
+//                 min_idx = j;
 
-        }
-        swap(arr, min_idx, i);
-    }
-}
-function printArray(arr, size) {
-    let i;
+//         }
+//         swap(arr, min_idx, i);
+//     }
+// }
+// function printArray(arr, size) {
+//     let i;
 
-    for (let i = 0; i < size; i++) {
-        console.log(arr[i] + " ");
-    }
-}
-let arr = [64, 25, 12, 22, 11,];
-let n = arr.length;
+//     for (let i = 0; i < size; i++) {
+//         console.log(arr[i] + " ");
+//     }
+// }
+// let arr = [64, 25, 12, 22, 11,];
+// let n = arr.length;
 
-selectionSort(arr, n);
-console.log("sorted Array : ");
-printArray(arr, n);
+// selectionSort(arr, n);
+// console.log("sorted Array : ");
+// printArray(arr, n);
+
+
+
+
+
+
+
