961f8036bccc1b1795c35b49cfc0747057cd2baf
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-17 06:39:38 +0530

    insertion sort

--- Sorting/selection sort/index.js
+++ Sorting/selection sort/index.js
@@ -75,27 +75,33 @@
 
 
 
-function insertionSort(arr, n) {
+// function insertionSort(arr, n) {
 
-    let key;
-    for (let i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+//     let key;
+//     for (let i = 1; i < n; i++) {
+//         key = arr[i];
+//         j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-function printArray(arr, n) {
+//         while (j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j = j - 1;
+//         }
+//         arr[j + 1] = key;
+//     }
+// }
+// function printArray(arr, n) {
 
-    for (let i = 0; i < n; i++)
-        console.log(arr[i]);
-}
-let arr = [12, 11, 13, 5, 6];
-let n = arr.length;
+//     for (let i = 0; i < n; i++)
+//         console.log(arr[i]);
+// }
+// let arr = [12, 11, 13, 5, 6];
+// let n = arr.length;
 
-insertionSort(arr, n);
-printArray(arr, n);
+// insertionSort(arr, n);
+// printArray(arr, n);
+
+
+
+
+
+
