7dc274bd3ea1fa6b94bc9b31dbeebf30aa122c3f
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-19 06:44:31 +0530

    revision

--- Searching/linear_search/index.js
+++ Searching/linear_search/index.js
@@ -1,3 +1,24 @@
+// function findElement(arr, n, x) {
+
+//     for (let i = 0; i < n; i++) {
+//         if (arr[i] == x) return i;
+//     }
+//     return -1;
+// }
+// let arr = [20, 12, 23, 3, 31, 30];
+// let n = arr.length;
+// let x = 31;
+
+// let result = findElement(arr, n, x);
+
+// (result == -1) ? console.log("Element is not Present in this Array") : console.log("Element Present in this Array : " + result);
+
+
+
+
+
+
+
 function findElement(arr, n, x) {
 
     for (let i = 0; i < n; i++) {
@@ -5,10 +26,10 @@ function findElement(arr, n, x) {
     }
     return -1;
 }
-let arr = [20, 12, 23, 3, 31, 30];
+let arr = [24, 34, 2, 34, 43, 54, 23, 4, 66, , 8];
 let n = arr.length;
-let x = 31;
+let x = 4;
 
 let result = findElement(arr, n, x);
 
-(result == -1) ? console.log("Element is not Present in this Array") : console.log("Element Present in this Array : " + result);
+(result == -1) ? console.log("Not Found") : console.log("Element Index is = " + result);
