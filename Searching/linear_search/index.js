fa1d298e367c18e7867d208608566e3b37af2fd3
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-09-23 06:28:38 +0530

    revision

--- Searching/linear_search/index.js
+++ Searching/linear_search/index.js
@@ -40,26 +40,54 @@
 
 
 
-function binarySearch(arr, x) {
+// function binarySearch(arr, x) {
 
-    let low = 0;
-    let high = arr.length - 1;
-    let mid;
+//     let low = 0;
+//     let high = arr.length - 1;
+//     let mid;
 
-    while (high >= low) {
-        mid = low + Math.floor((high - low) / 2);
+//     while (high >= low) {
+//         mid = low + Math.floor((high - low) / 2);
 
-        if (arr[mid] == x) return mid;
+//         if (arr[mid] == x) return mid;
+
+//         if (arr[mid] > x)
+//             high = mid - 1;
+
+//         else low = mid + 1;
+//     }
+//     return -1;
+// }
+// let arr = [2, 4, 6, 7, 9, 10, 12, 14];
+// let x = 14;
+// let result = binarySearch(arr, x);
+
+// (result == -1) ? console.log("Not Found") : console.log("Element Present in Index : " + result);
+
+
+
+
+
+
+
+function binarySearch(arr, l, r, x) {
+
+    if (r >= l) {
+        let mid = l + Math.floor((r - l) / 2);
+
+        if (arr[mid] == x)
+            return mid;
 
         if (arr[mid] > x)
-            high = mid - 1;
+            return binarySearch(arr, l, mid - 1, x);
 
-        else low = mid + 1;
+        return binarySearch(arr, mid + 1, r, x);
     }
     return -1;
 }
-let arr = [2, 4, 6, 7, 9, 10, 12, 14];
-let x = 14;
-let result = binarySearch(arr, x);
+let arr = [1, 2, 3, 5, 6, 7, 8, 9];
+let x = 3;
+let n = arr.length;
+let result = binarySearch(arr, 0, n - 1, x);
 
-(result == -1) ? console.log("Not Found") : console.log("Element Present in Index : " + result);
+(result == -1) ? console.log("not Found") : console.log("Element Present In Index = " + result);
