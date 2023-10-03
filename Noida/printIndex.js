e853fb0ea69c9bc2c8900d8c6c7d4d5117fb8eba
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-10-03 16:42:06 +0530

    print

---
+++ Noida/printIndex.js
@@ -0,0 +1,20 @@
+// JavaScript program to find indices of all
+// occurrences of one String in other.
+function printIndex(str, s) {
+    var flag = false;
+    for (var i = 0; i < str.length - s.length + 1; i++) {
+        if (str.substring(i, s.length + i) == s) {
+            console.log(i + " ");
+            flag = true;
+        }
+    }
+
+    if (flag === false) {
+        console.log("NONE");
+    }
+}
+
+// Driver code
+var str1 = "GeeksforGeeks";
+var str2 = "Geeks";
+printIndex(str1, str2);
